76a47fdc353317cf1ecc80a0e8d2316c1605ebf7
Phillip Sturm <sturmphillip95@gmail.com>
2019-01-22 21:03:57 -0600

    revised app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,42 +7,42 @@
     var questions = [
         {
             question: "1) Which of the following is not a favorable adjective when discussing wine?",
-            answers: ["Fat", "Flinty","Leggy", "Vigorous"],
+            answers: ["  Fat  ", "  Flinty  ","  Leggy  ", "  Vigorous  "],
             correctAnswer: "Fat"
         },
         {
             question: "2) Which phrase is used in restaurants to refer to being out of an item? ",
-            answers: ["404", "86", "Slim", "56"],
+            answers: ["  404  ", "  86  ", "  Slim  ", "  56  "],
             correctAnswer: "86"
         },
         {
             question: "3) Which ingredient is not essential in a margarita?",
-            answers: ["Orange Liqueur", "Tequila", "Simple Syrup", "Lime Juice"],
+            answers: ["  Orange Liqueur  ", "  Tequila  ", "  Simple Syrup  ", "  Lime Juice  "],
             correctAnswer: "Simple Syrup"
         },
         {
             question: "4) What year did the oldest restaurant in Chicago Open?",
-            answers: ["1909", "1907", "1881", "1892"],
+            answers: ["  1909  ", "  1907  ", "  1881  ", "  1892  "],
             correctAnswer: "1881"
         },
         {
             question: "5) What is Huitlacoche?",
-            answers: ["Argentinian Candy", "Brazilian Fish", "Peruvian Root Vegetable", "Mexican Truffle"],
+            answers: ["  Argentinian Candy  ", "  Brazilian Fish  ", "  Peruvian Root Vegetable  ", "  Mexican Truffle  "],
             correctAnswer: "Mexican Truffle"
         },
         {
             question: "6) Which apple variety was designed at the University of Minnesota?",
-            answers: ["Fuji", "Honeycrisp", "McIntosh Red", "Golden Delicious"],
+            answers: ["  Fuji  ", "  Honeycrisp  ", "  McIntosh Red  ", "  Golden Delicious  "],
             correctAnswer: "Honeycrisp"
         },
         {
             question: "7) Which country produces the most potatoes?",
-            answers: ["United States", "China", "Ireland", "Russia"],
+            answers: ["  United States  ", "  China  ", "  Ireland  ", "  Russia  "],
             correctAnswer: "China"
         },
         {
             question: "8) In the United States, about how much beer does the average person drink each year?",
-            answers: ["24 Pints","24 Quarts", "24 Gallons", "24 Gills"],
+            answers: ["  24 Pints  ","  24 Quarts  ", "  24 Gallons  ", "  24 Gills  "],
             correctAnswer: "24 Gallons"
         }
 
@@ -59,17 +59,16 @@
         $('#start').remove();
         intervalId = setInterval(countdown, 1000);
 
-        $('#wrapper').prepend(`<h2> Time Remaining:<span id="counter-number"> ${counter}</span>  Seconds</h2>`);
+        $('#timer').prepend(`<h2> Time Remaining: <span id="counter-number"> ${counter}</span>  Seconds</h2>`);
 
         for (var i = 0; i < questions.length; i++) {
-            $("#button").append('<h2>' + questions[i].question + '</h2>');
+            $("#mainBody").append('<h2>' + questions[i].question + '</h2>');
             for (var j = 0; j < questions[i].answers.length; j++) {
-                $("#button").append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
+                $("#mainBody").append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
             }
         }
 
-        $("#button").append('<button id="done">Done</button>');
-        $("#button").setAttribute("button");
+        $("#donebtn").append('<button id="done">Done</button>');
     }
     function countdown() {
         counter--;
@@ -92,11 +91,11 @@
         }
 
         clearInterval(intervalId);
-        $("#button").empty();
-        $('#wrapper h2').remove();
-        $('#button').append('<h3>Correct Answers: ' + correct + '</h3>');
-        $('#button').append('<h3>Incorrect Answers: ' + inCorrect + '</h3>');
-        $('#button').append('<h3>Unanswered: ' + (questions.length - (inCorrect + correct)) + '</h3>');
+        $("#mainBody").empty();
+        $('#timer h2').remove();
+        $('#mainBody').append('<h3>Correct Answers: ' + correct + '</h3>');
+        $('#mainBody').append('<h3>Incorrect Answers: ' + inCorrect + '</h3>');
+        $('#mainBody').append('<h3>Unanswered: ' + (questions.length - (inCorrect + correct)) + '</h3>');
     }
 
 });
